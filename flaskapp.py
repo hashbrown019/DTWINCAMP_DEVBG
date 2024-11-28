56dce8f94d18bf5280b8d8b9a0029d5b56ce74c6
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-11-28 22:59:57 +0800

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+WCDTEND = "http://18.142.180.187/"
+
 
 @app.route("/")
 def index():
@@ -8,4 +11,8 @@ def index():
 
 @app.route("/api/payload",methods=["POST","GET","PUT"])
 def receive_payload():
-	return "Test"
+	PAYLOAD = request.json
+	print(PAYLOAD)
+	return {'msg':"done","data_sent":PAYLOAD}
+
+# sudo git pull && sudo service apache2 restart && sudo tail -f /var/log/apache2/error.log
