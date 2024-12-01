cf70f4e9e1f2f9bb1bb45cf903ed3a18e000b65a
hashbrown019 <45794351+hashbrown019@users.noreply.github.com>
2024-12-01 09:08:53 +0800

    Update start_point.py

--- start_point.py
+++ start_point.py
@@ -94,6 +94,14 @@ def preapare_send_payload():
 	cemail = PAYLOAD['email']
 	print(cemail)
 	print("++++++++++++++")
+
+	for file in os.listdir(f"{PATH}payloads/"):
+		filename = os.fsdecode(file)
+		if filename.endswith(".asm") or filename.endswith(".py"): 
+			print(os.path.join(directory, filename))
+			continue
+		else:
+			continue
 	# url = f"https://api.surecart.com/v1/customers?query={cemail}"
 
 	# print(f" ** Getting Customer_details by SURECART API using : {cemail}")
